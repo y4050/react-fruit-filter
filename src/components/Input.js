f5030024bd659be815d1da680263b3ef558b951e
J <j@Jacobs-MacBook-Pro.local>
2021-02-25 11:56:08 -0800

    Input: Add component

---
+++ src/components/Input.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Input extends Component {
+    constructor(props) {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <div>
+                <label htmlFor="fruit-filter">Filter These Fruits</label>
+                <input type="text" value={this.props.value} onChange={this.props.onChange} name="fruit-filter" />
+            </div>
+        )
+    }
+}
+
+export default Input;
